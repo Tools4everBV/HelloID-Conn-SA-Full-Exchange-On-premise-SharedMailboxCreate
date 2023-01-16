ebda47eb216032ecd4646e3fa540160581d2adc3
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2023-01-16 15:40:57 +0100

    Changed new-object to ::new()

--- Manual resources/[task]_Exchange on-premise - Create new shared mailbox.ps1
+++ Manual resources/[task]_Exchange on-premise - Create new shared mailbox.ps1
@@ -11,7 +11,7 @@ $upn = $form.naming.userPrincipalName
 # Connect to Exchange
 try{
     $adminSecurePassword = ConvertTo-SecureString -String "$ExchangeAdminPassword" -AsPlainText -Force
-    $adminCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ExchangeAdminUsername,$adminSecurePassword
+    $adminCredential = [System.Management.Automation.PSCredential]::new($ExchangeAdminUsername,$adminSecurePassword)
     $sessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
     $exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $exchangeConnectionUri -Credential $adminCredential -SessionOption $sessionOption -ErrorAction Stop 
     #-AllowRedirection
